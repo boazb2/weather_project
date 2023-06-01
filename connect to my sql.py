9f57ac83b8f4a72dde668213560cccca78fa0600
Boaz Bokek <boazbokek@gmail.com>
2023-06-01 17:12:38 +0000

    Adding the connection to mysql

---
+++ connect to my sql.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import mysql.connector
+
+# Connect to the MySQL database
+conn = mysql.connector.connect(
+    host='cnt7-naya-cdh63',
+    user='nifi',
+    password='NayaPass1!',
+    database='weather_db'
+)
+
+# Query to fetch data from the table
+query = "SELECT * FROM Users"
+
+# Fetch data into a pandas DataFrame
+df = pd.read_sql_query(query, conn)
+
+print(df)
