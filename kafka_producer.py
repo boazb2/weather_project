f0ac0b196e43e0472a87a488480dbcee716f9b03
Boaz Bokek <boazbokek@gmail.com>
2023-06-15 18:00:43 +0000

    gggg

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,4 +1,5 @@
 from kafka import KafkaProducer
+import pandas as pd
 import json
 import get_weather_from_api as weather
 import get_cities_from_db as gc
@@ -7,9 +8,14 @@ api_key = "d4593bcce04eda71196c6bcf156b3eea"
 
 # Getting the required cities for a user request
 
-cities_list = gc.get_cities_array("SELECT city_name FROM users_cities")
+cities_list = gc.get_cities_array("SELECT distinct city_name FROM users_cities")
+users_cities_df = gc.get_users_cities("SELECT uc.bot_user_id,uc.city_name,u.first_name,u.last_name FROM users_cities uc join users u on uc.bot_user_id = u.bot_user_id")
 
 weather_data = weather.get_weather_data(api_key, cities_list)
+
+weather_data_df  = pd.DataFrame([(key, weather_data[key]['main']['feels_like']) for key in weather_data.keys()], columns=['city_name', 'feels_like'])
+
+merged_df = pd.merge(users_cities_df, weather_data_df, on='city_name', how='inner')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 bootstrap_servers = 'cnt7-naya-cdh63:9092'  # Replace with the appropriate broker address
@@ -18,10 +24,16 @@ topic_name = 'weather_topic'  # Replace with the name of the Kafka topic
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
 # Send JSON object as a message
-message = json.dumps(weather_data).encode('utf-8')
+message = json.dumps(merged_df.to_json(orient='records')).encode('utf-8')
 producer.send(topic_name, value=message)
 
+
 #print(message)
+#print(users_cities_df)
+#print(weather_data_df)
+
+print(merged_df.to_json(orient='records'))
+
 
 # Flush and close the producer
 producer.flush()
--- sentmessage.py
+++ sentmessage.py
@@ -4,23 +4,21 @@ from telegram.ext import Updater
 def notify_user(bot, df, user_id):
     # Tell the user about df
     bot.send_message(chat_id=user_id, text="Something important happened! Here is the data:\n\n{}".format(df))
+    
 
-def main():
+def main(p_user_id,p_tk,p_df):
     # Set up your updater and dispatcher as before
-    updater = Updater(token="5993334898:AAHCOt1GTVEW3_0FY-wEWmyMvnt71VceloM", use_context=True)
+    updater = Updater(token=p_tk, use_context=True)
     dispatcher = updater.dispatcher
 
     # Your code to detect the important event and get the relevant data and user ID
-    df = []
-    user_id = '483699123'  # Replace with the desired user's chat ID
+    df = p_df
+    user_id = p_user_id  # Replace with the desired user's chat ID
 
-    # Call the notify_user function with the relevant parameters
-    notify_user(updater.bot, df, user_id)
-
+   
     # Start the bot
     updater.start_polling()
     updater.idle()
 
-# Run the main function
-if __name__ == '__main__':
-    main()
+ # Call the notify_user function with the relevant parameters
+    notify_user(updater.bot, df, user_id)
